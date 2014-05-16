3a5e30c81a58a877f0279d39e712aebb447a9377
John Lekberg <jclekberg@gmail.com>
2014-05-15 22:30:33 -0500

    Formatted the Font class
    Formatted the code of the Font class, but keep functionality the same.

--- Graphics/Font.java
+++ Graphics/Font.java
@@ -1,31 +1,35 @@
 package Graphics;
 
 /**
- * Write a description of class Font here.
+ * Holds the methods pertaining to rendering text on the game screen.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Lukas Muñoz, Luke Staunton, JCL
  */
-public class Font
-{
-    public static boolean notOK = false;
-    private static String chars = "" +
+public final class Font {
+    public static boolean ok = true;
+    private static final String legalCharacters = // contains all the characters that can be rendered
         "ABCDEFGHIJKLMNOPQRSTUVWXYZ      " + 
         "0123456789.,:;'\"!?$%()-=+/_     "; 
 
-    public static void render(String msg, Screen screen, int x, int y, int color, int scale)
-    {
-        if(!notOK)
-        {
-            msg = msg.toUpperCase();
-            for(int i = 0; i < msg.length(); i++)
-            {
-                int charIndex = chars.indexOf(msg.charAt(i));
-                if(charIndex >= 0)
-                {
-                    screen.render(x + i*8*scale, y, charIndex + 30 * 32, color, 0x00, scale);
+    /**
+     * Renders the message 'msg on the Screen 'screen at the coordinates ('x, 'y) with a color 'color
+     * and at a scale 'scale.
+     */
+    public static void render(final String msg, final Screen screen, final int x, final int y, final int color, final int scale) {
+        if(ok) {
+            final String uppercaseMsg = msg.toUpperCase();
+            for(int iter = 0; iter < uppercaseMsg.length(); ++iter) {
+                final int charIndex = legalCharacters.indexOf(uppercaseMsg.charAt(iter));
+                if(isInString(charIndex)) {
+                    screen.render(x + iter*8*scale, y, charIndex + 30 * 32, color, 0x00, scale);
                 }
             }
         }
     }
+    
+    /**
+     * Returns true if the index 'charIndex for the a char in the string 'chars is NOT -1,
+     * indicating that the char is in the string
+     */
+    private static final boolean isInString(int charIndex) { return charIndex != -1; }
 }
